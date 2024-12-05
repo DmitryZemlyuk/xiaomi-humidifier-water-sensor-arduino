7c8853e450464b9ae19be50a6fb7a7847d02d0db
Dmitry Zemlyuk <d.zemlyuk@gmail.com>
2024-12-05 15:22:53 +0200

    update(HumidifierSensor.ino, README.md): add readme, add fix for stop fan

---
+++ HumidifierSensor.ino
@@ -0,0 +1,67 @@
+#include <CapacitiveSensor.h>
+
+#define SENDER_PIN 3
+#define SENSOR_PIN 2
+
+#define SAMPLES_NUMBER 232
+#define TIMEOUT_MS 500
+
+#define MIN_READING 3900   // for 1M resistor and 1000 samples
+#define MAX_READING 13100  // for 1M resistor and 1000 samples
+#define PACKET_SIZE 42
+#define CHECKSUM_BASE 0xA0
+#define FULL_TANK 125
+#define FAN_OFF 127
+#define TAKEAWAY 2700
+
+unsigned long previousMillis = 0;
+const long interval = 200;
+
+byte packet[] = { 0xFA, 0x29, 0x03, 0x00, 0x00, 0x00, 0x00, 0x14, 0x9A, 0x00, 0x00, 0x00, 0x03, 0x77, 0x72, 0x71, 0x03, 0x00, 0x6C, 0x4C, 0x3B, 0x03, 0x2F, 0x15, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x21, 0x00, 0x00, 0x03, 0xE8, 0x00, 0x2c, 0x02, 0x6D, 0x37, 0xD2, 0x00 };
+
+byte checksum;
+byte waterLevel;
+long readingRaw;
+
+CapacitiveSensor sensor = CapacitiveSensor(SENDER_PIN, SENSOR_PIN);  // 1M resistor between pins 3 & 2, pin 2 is sensor pin
+
+void setup() {
+  sensor.set_CS_AutocaL_Millis(0xFFFFFFFF);  // turn off autocalibrate
+  sensor.set_CS_Timeout_Millis(TIMEOUT_MS);
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    checksum = 0;
+    packet[11] = 0;
+
+    readingRaw = sensor.capacitiveSensorRaw(SAMPLES_NUMBER);
+
+    if (readingRaw == -2) waterLevel = FULL_TANK;  // // tank full, water touches both metal probes, capacitance measurement not possible
+
+    else if (readingRaw > TAKEAWAY && readingRaw < MIN_READING) waterLevel = 127;  // turn off fan, available if air dry mode turned off, please check in Mi Home app
+
+    else waterLevel = constrain(map(readingRaw, MIN_READING, MAX_READING, 0, 120), 0, 120);  // map sensor reading
+
+    packet[11] = waterLevel;  // 12th byte represents water level value
+
+    checksum = calculateChecksum(packet, PACKET_SIZE);
+    packet[PACKET_SIZE] = checksum;  // last byte checksum
+
+    Serial.write(packet, sizeof(packet));
+    // Serial.println(readingRaw); // for calibration - raw value from water tank
+    // Serial.println(packet[11]); // for calibration - water level for send to head
+  }
+}
+
+byte calculateChecksum(byte* packet, int size) {
+  byte checksum = 0;
+  for (int i = 0; i < size; i++) {
+    checksum ^= packet[i];
+  }
+  checksum ^= CHECKSUM_BASE;
+  return checksum;
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # xiaomi-humidifier-water-sensor-arduino
-Xiaomi SmartMi Evaporative Humidifier water level sensor CJXJSQ02ZM, CJXJSQ04ZM
+Xiaomi SmartMi Evaporative Humidifier Water Level Sensor CJXJSQ02ZM, CJXJSQ04ZM for Arduino Nano
+
+Special thanks to Tomas Vilda for his excellent work reverse-engineering the water sensor's serial communication: [GitHub link](https://github.com/tomasvilda/humidifierstart)
+
+This project is based on: [GitHub link](https://github.com/fakcior/xiaomi-humidifier-water-sensor/tree/master?tab=readme-ov-file)
+
+Fixes:
+
+- Added functionality to stop the fan and disk drive when the top of the humidifier is removed (this works when "Air Dry" mode is turned OFF; so please check this switch in the Mi Home app).
+- Improved performance by replacing delay(); with millis(); and other optimizations.
+
+This sketch uses the Capacitive Touch Library (CapacitiveSensor.h) to utilize the existing hardware.
+
+To set up:
+
+- Solder a 1MΩ resistor between two pins on your board (in this sketch, pins D2 and D3 are used).
+- Connect or solder your board as a direct replacement for the blue sensor module on the power supply PCB (5V, GND, TX, and both probes). Looking from the top of the 2-pin header, the left pin is the sensing pin, and the right one is GND.
+
+This sketch also works for the black sensor module in the CJXJSQ04ZM model (located near the power input connector).
+Calibration: Calibrate your board under three conditions:
+
+- When the top of the humidifier is removed
+- When the humidifier is fully assembled with an empty tank
+- When the tank is full.
+Adjust the TAKEAWAY, MIN_READING, and MAX_READING definitions accordingly based on the sensor readings (use Serial.println(readingRaw); to monitor values).
+
+Important: It is recommended to calibrate the sensor by slowly filling the tank with water while monitoring the sensor values. Record the maximum value. When the water reaches a certain level, it will short both metal rods, and the measurement will return -2, which is expected behavior.
