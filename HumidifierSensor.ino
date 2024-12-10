9cb117f981edd89ff340dec89341bdf2b487fe38
Dmitry Zemlyuk <d.zemlyuk@gmail.com>
2024-12-10 17:57:37 +0200

    update(HumidifierSensor.ino): change interval value, replace map, memory usage ~3980 bytes

--- HumidifierSensor.ino
+++ HumidifierSensor.ino
@@ -14,18 +14,18 @@ const byte FULL_TANK = 125;
 const byte FAN_OFF = 127;
 const int TAKEAWAY = 2700;
 
-const long INTERVAL = 100;
+const long INTERVAL = 500;
 
-byte packet[PACKET_SIZE + 1] = { 
-  0xFA, 0x29, 0x03, 0x00, 0x00, 0x00, 0x00, 0x14, 0x9A, 0x00, 0x00, 0x00, 
-  0x03, 0x77, 0x72, 0x71, 0x03, 0x00, 0x6C, 0x4C, 0x3B, 0x03, 0x2F, 0x15, 
-  0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x21, 0x00, 0x00, 0x03, 0xE8, 
-  0x00, 0x2c, 0x02, 0x6D, 0x37, 0xD2, 0x00 
+byte packet[PACKET_SIZE + 1] = {
+  0xFA, 0x29, 0x03, 0x00, 0x00, 0x00, 0x00, 0x14, 0x9A, 0x00, 0x00, 0x00,
+  0x03, 0x77, 0x72, 0x71, 0x03, 0x00, 0x6C, 0x4C, 0x3B, 0x03, 0x2F, 0x15,
+  0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x21, 0x00, 0x00, 0x03, 0xE8,
+  0x00, 0x2c, 0x02, 0x6D, 0x37, 0xD2, 0x00
 };
 
 unsigned long previousMillis = 0;
-byte waterLevel = 0;
-long readingRaw = 0;
+byte waterLevel = 120;
+long readingRaw = MAX_READING;
 
 CapacitiveSensor sensor = CapacitiveSensor(SENDER_PIN, SENSOR_PIN);  // 1M resistor between pins 3 & 2, pin 2 is sensor pin
 
@@ -46,13 +46,12 @@ void loop() {
     // Determine water level based on readingRaw
     if (readingRaw == -2) {
       waterLevel = FULL_TANK;  // Tank full, water touches both metal probes
-    }
-    else if (readingRaw > TAKEAWAY && readingRaw < MIN_READING) {
+    } else if (readingRaw > TAKEAWAY && readingRaw < MIN_READING) {
       waterLevel = FAN_OFF;  // Turn off fan (air dry mode off)
-    }
-    else {
+    } else {
       // Map sensor reading to water level
-      waterLevel = constrain(map(readingRaw, MIN_READING, MAX_READING, 0, 120), 0, 120);
+      waterLevel = ((readingRaw - MIN_READING) * 120L) / (MAX_READING - MIN_READING);
+      waterLevel = constrain(waterLevel, 0, 120);
     }
 
     // Update packet with new water level and calculate checksum
